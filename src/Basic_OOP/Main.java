80cc2c8a3f3ac3d03a9a16e80b5613031932a2ef
Himaglahi <mjobsimagsalos@gmail.com>
2023-11-17 08:59:16 +0800

    SampleProject

---
+++ src/Basic_OOP/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Basic_OOP;
+
+/**
+ *
+ * @author PC
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Basic_OOP");
+    }
+    
+    
+}
